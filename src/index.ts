09145737a8fdf6057dc0620eceda8da76d365bb7
hejilun <hejilun@yy.com>
2020-12-29 18:18:05 +0800

    fix: export

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ import createScopeStore from './createScopeStore'
 import Provider, {withStores} from './Provider'
 
 export {createScopeStore, Provider, withStores}
+export default {createScopeStore, Provider, withStores}
